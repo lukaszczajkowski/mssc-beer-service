f2df7024e0b6a5f96325c48673a7835bab344b16
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-07-28 11:07:33 +0200

    Junit 5 tests added

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -3,7 +3,6 @@ package guru.springframework.msscbeerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//initial
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
