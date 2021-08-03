dda5bdc56240e5d19e7f682b8fb9e1be5f2aa5b9
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-03 11:02:43 +0200

    hibernate error fixed

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,9 +2,10 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
 //initial
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
