6f17aed721d7d93a4523087f2be0d5c352e62072
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-05 22:22:50 +0200

    Beer events objects created

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -8988748029383126838L;
+
+    private final BeerDto beerDto;
+}
